c6bc0732272800d53ead60220942d412f3aec5e3
MAREKOR555 <MAREKOR555@INTERIA.PL>
2025-07-22 17:34:43 +0200

    add help command, project mostly finished

--- main.go
+++ main.go
@@ -20,8 +20,16 @@ func handleMsg(msg string, err error, cleanup func()) {
 }
 
 func main() {
-	mainCmd := os.Args[1]
-	extraArgs := os.Args[2:]
+	var extraArgs []string
+	var mainCmd string
+	if len(os.Args) < 2 {
+		mainCmd = "help"
+		fmt.Println("No command provided, defaulting to help")
+	} else {
+		mainCmd = os.Args[1]
+		extraArgs = os.Args[2:]
+	}
+
 	switch mainCmd {
 	case "list", "l":
 		files, msg, err := listArchive(extraArgs[0])
@@ -90,6 +98,16 @@ func main() {
 		unpackMsg, err := unpackArchive(strings.TrimSuffix(extraArgs[0], ".zst"), unPackPrefix)
 		handleMsg("check if file has .tbaf.zst extension\n"+unpackMsg, err, cleanup)
 		cleanup()
+	case "help", "h":
+		fmt.Println("tbaf usage:")
+		fmt.Println("	tbaf list <archive.tbaf> - List files in the archive")
+		fmt.Println("	tbaf build <archive.tbaf> <file1> <file2> ... - Build an archive with the specified files")
+		fmt.Println("	tbaf unpack <archive.tbaf> <prefix> - Unpack the archive to the specified prefix")
+		fmt.Println("	tbaf compress <archive.tbaf> - Compress the archive using zstd")
+		fmt.Println("	tbaf decompress <archive.tbaf.zst> - Decompress the archive using zstd")
+		fmt.Println("	tbaf build-compress <archive.tbaf.zst> <file1> <file2> ... - Build and compress an archive")
+		fmt.Println("	tbaf unpack-decompress <archive.tbaf.zst> <prefix> - Unpack and decompress the archive")
+		fmt.Println("	tbaf help - Show this help message")
 	default:
 		fmt.Println("command not found")
 	}
